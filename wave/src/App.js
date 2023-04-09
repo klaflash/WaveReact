8cdf7159b8e0b1e2a9fe80c421cf2d7e64184130
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-04-09 00:19:16 +0000

    Browser to Hash Router

--- wave/src/App.js
+++ wave/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import MainPage from './MainPage';
 import LocationPage from './LocationPage';
 
