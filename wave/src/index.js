4cc4a53513b7d3466d91d571544ff70446331454
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-04-09 04:53:53 +0000

    Setting up for Supabase

--- wave/src/index.js
+++ wave/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
 import './style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// import { createClient } from '@supabase/supabase-js'
+// const API_KEY = process.env.REACT_APP_API_KEY
+// const PROJECT_URL = process.env.REACT_APP_PROJECT_URL
+// const supabase = createClient(`${PROJECT_URL}`, `${API_KEY}`)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
