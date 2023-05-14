42c5abe6612b911cdf93e67a2033ace0e497f825
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-05-14 02:01:02 +0000

    Added filter by top and near me to main page

--- wave/src/MainPage.js
+++ wave/src/MainPage.js
@@ -36,10 +36,20 @@ const updateAverages = async () => {
   return averages;
 };
 
+// const getFilteredLocations = (locations, selectedDistance, searchQuery) => {
+//   const filteredLocations = selectedDistance >= 0 ? locations.filter((location) => location.dist * 0.621371 <= selectedDistance) : locations;
+//   if (searchQuery.trim() !== '') {
+//     const query = searchQuery.toLowerCase().trim();
+//     return filteredLocations.filter(location => location.name.toLowerCase().includes(query) || location.addy.toLowerCase().includes(query));
+//   }
+//   return filteredLocations;
+// }
+
 
 function MainPage(props) {
   const [isMobile, setIsMobile] = useState(false);
-  const threshold = 0.09;
+  //const threshold = 0.09;
+  const threshold = 100;
   const locations = useMemo(() => [
     { name: 'Nova', latitude: 40.415171, longitude: -86.893275, addy: '200 S Fourth St'},
     { name: 'Hub', latitude: 40.422203, longitude: -86.906227, addy: '111 S Salisbury St'},
@@ -56,6 +66,32 @@ function MainPage(props) {
     const isMobileDevice = /Mobi/.test(navigator.userAgent);
     setIsMobile(isMobileDevice);
   }, [props, locations, handleLocationClick, props.inRange]);
+
+  const [averages, setAverages] = useState({});
+  const [mostRecent, setMostRecent] = useState({});
+
+  useEffect(() => {
+    const getAverages = async () => {
+      const firstAverages = await updateAverages();
+      const secondAverages = await updateAverages();
+      setAverages(secondAverages[0]);
+      setMostRecent(secondAverages[1])
+    };
+    getAverages();
+  
+    const intervalId = setInterval(async () => {
+      const newAverages = await updateAverages();
+      setAverages(newAverages[0]);
+      setMostRecent(newAverages[1])
+    }, 10000);
+  
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  console.log(averages)
+  console.log(mostRecent)
 
   useEffect(() => {
     const degToRad = (deg) => {
@@ -83,8 +119,24 @@ function MainPage(props) {
         }
       }
       props.setInRange(newInRange);
-      
-      const sortedLocations = locations.sort((a, b) => a.dist - b.dist);
+
+      //const sortedLocations = locations.sort((a, b) => a.dist - b.dist);
+      console.log(averages)
+      console.log(sortOrder)
+      const sortedLocations = [...locations].sort((a, b) => {
+        if (sortOrder === 'desc') {
+          if (!averages[b.name]) {
+            return -1;
+          } else if (!averages[a.name]) {
+            return 1;
+          } else {
+            return averages[b.name]['averageScore'] - averages[a.name]['averageScore'];
+          }
+        } else {
+          return a.dist - b.dist;
+        }
+      });
+      setFilteredLocations(sortedLocations);
       props.setLocations(sortedLocations)
     };
 
@@ -101,32 +153,6 @@ function MainPage(props) {
 
   }, [locations, props.setInRange, props.setLocations, threshold]);
 
-  const [averages, setAverages] = useState({});
-  const [mostRecent, setMostRecent] = useState({});
-
-  useEffect(() => {
-    const getAverages = async () => {
-      const firstAverages = await updateAverages();
-      const secondAverages = await updateAverages();
-      setAverages(secondAverages[0]);
-      setMostRecent(secondAverages[1])
-    };
-    getAverages();
-  
-    const intervalId = setInterval(async () => {
-      const newAverages = await updateAverages();
-      setAverages(newAverages[0]);
-      setMostRecent(newAverages[1])
-    }, 10000);
-  
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-  
-
-  console.log(averages)
-  console.log(mostRecent)
 
   async function updateAverages() {
     const { data: ratings, error } = await supabase
@@ -209,17 +235,138 @@ function MainPage(props) {
     return results;
   }
 
+  const [sortOrder, setSortOrder] = useState(localStorage.getItem('sortOrder') || 'loc');
+  const [selectedDistance, setSelectedDistance] = useState(parseInt(localStorage.getItem('selectedDistance')) || 10);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredLocations, setFilteredLocations] = useState([])
+
+  useEffect(() => {
+    const sortedLocations = [...locations].sort((a, b) => {
+      console.log('averages LOWER')
+      console.log(averages)
+      if (sortOrder === 'desc') {
+        if (!averages[b.name]) {
+          return -1;
+        } else if (!averages[a.name]) {
+          return 1;
+        } else {
+          return averages[b.name]['averageScore'] - averages[a.name]['averageScore'];
+        }
+      } else {
+        return a.dist - b.dist;
+      }
+    });
+
+    console.log('Now sorted')
+    console.log(sortedLocations)
+  
+    setFilteredLocations(sortedLocations);
+  }, [sortOrder]);
+  
+  
+
+  // useEffect(() => {
+  //   // This code will run once when the component mounts
+  //   console.log("IS IT EVEN RUNNING")
+  //   const locationsByDistance = [...locations].sort((a, b) => {
+  //     if (sortOrder === 'desc') {
+  //       return (averages && averages[b.name] && averages[b.name]['averageScore']) - (averages && averages[a.name] && averages[a.name]['averageScore']);
+  //     } else {
+  //       return (a.dist - b.dist);
+  //     }
+  //   });
+  //   setFilteredLocations(locationsByDistance);    
+  //   console.log(locationsByDistance)
+  //   console.log("DONE RUNNING")
+  // }, [locations, averages, sortOrder]);
+
+
+  // const sortedLocations = useMemo(() => {
+  //   return [...locations].sort((a, b) => {
+  //     if (sortOrder === 'desc') {
+  //       return (averages && averages[b.name] && averages[b.name]['averageScore']) - (averages && averages[a.name] && averages[a.name]['averageScore']);
+  //     } else {
+  //       return (a.dist - b.dist);
+  //     }
+  //   });
+  // }, [locations, averages, sortOrder]);
+
+  // useEffect(() => {
+  //   const locationsByDistance = selectedDistance >= 0 ? sortedLocations.filter((location) => location.dist * 0.621371 <= selectedDistance) : sortedLocations;
+  //   setFilteredLocations(locationsByDistance);
+  // }, [sortedLocations, selectedDistance]);
+
+  // useEffect(() => {
+  //   const locationsByDistance = selectedDistance >= 0 ? sortedLocations.filter((location) => location.dist * 0.621371 <= selectedDistance) : sortedLocations;
+
+  //   if (searchQuery.trim() !== '') {
+  //     const query = searchQuery.toLowerCase().trim();
+  //     const filtered = locationsByDistance.filter(location => location.name.toLowerCase().includes(query) || location.addy.toLowerCase().includes(query));
+  //     setFilteredLocations(filtered);
+  //   } else {
+  //     setFilteredLocations(locationsByDistance);
+  //   }
+  // }, [sortedLocations, selectedDistance, searchQuery]);
+
+  useEffect(() => {
+    localStorage.setItem('sortOrder', sortOrder);
+  }, [sortOrder]);
+
+  // useEffect(() => {
+  //   localStorage.setItem('selectedDistance', selectedDistance);
+  // }, [selectedDistance]);
+
+  // const handleDistanceChange = (e) => {
+  //   const newSelectedDistance = parseInt(e.target.value);
+  //   setSelectedDistance(newSelectedDistance);
+  //   localStorage.setItem('selectedDistance', newSelectedDistance);
+  // };
+
+  // const handleSearchChange = (event) => {
+  //   setSearchQuery(event.target.value);
+  // };
+
+
+
   return (
     <div>
+      {/* 
+      <div>
+        <label htmlFor="search-input">Search:</label>
+        <input type="text" id="search-input" placeholder='Search' onChange={handleSearchChange} value={searchQuery} />
+      </div>
+      <div>
+        <label htmlFor="distance-select">Show locations within:</label>
+        <select id="distance-select" onChange={handleDistanceChange} value={selectedDistance}>
+          <option value="10">10 mi</option>
+          <option value="20">20 mi</option>
+          <option value="50">50 mi</option>
+          <option value="-1">All</option>
+        </select>
+      </div>
+      */}
+      <button className={sortOrder === 'desc' ? 'blue-button' : 'normal-button'} onClick={() => {setSortOrder(sortOrder === 'desc' ? 'desc' : 'desc'); localStorage.setItem('sortOrder', 'desc')}}>
+        Top
+      </button>
+      <button className={sortOrder === 'loc' ? 'blue-button' : 'normal-button'} onClick={() => {setSortOrder(sortOrder === 'loc' ? 'loc' : 'loc'); localStorage.setItem('sortOrder', 'loc')}}>
+        Near Me
+      </button>
       {/* {!isMobile ? */}
         <div id="content">
           <div id="header">
             <div id='logo'>Wave</div>
           </div>
           <ul>
-            {locations.map((location) => (
+            {filteredLocations.map((location) => (
               <li key={location.name}>
-                <Link className='button-link' to={`/location/${location.name}?inRange=${encodeURIComponent(JSON.stringify(props.inRange[location.name]))}`} onClick={() => handleLocationClick(location.name)}>
+                <Link className='button-link' to={`/location/${location.name}?inRange=${encodeURIComponent(JSON.stringify(props.inRange[location.name]))}`} onClick={() => handleLocationClick(location.name)} style={{backgroundColor: 
+                  averages && averages[location.name] && averages[location.name]['averageScore'] >= 0 && averages[location.name]['averageScore'] <= 2 ? 'red' :
+                  averages && averages[location.name] && averages[location.name]['averageScore'] > 2 && averages[location.name]['averageScore'] <= 4 ? 'orange' :
+                  averages && averages[location.name] && averages[location.name]['averageScore'] > 4 && averages[location.name]['averageScore'] <= 6 ? 'yellow' :
+                  averages && averages[location.name] && averages[location.name]['averageScore'] > 6 && averages[location.name]['averageScore'] <= 8 ? 'green' :
+                  averages && averages[location.name] && averages[location.name]['averageScore'] > 8 && averages[location.name]['averageScore'] <= 10 ? 'blue' :
+                  ''
+                }}>
                   <div className='card-left'>
                     <div className='bar-name'>{location.name}</div>
                     <div className='bar-addy'>{location.addy}</div>
