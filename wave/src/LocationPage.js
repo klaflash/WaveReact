d4b1bddeb8bffebf0a5e3c336f840bbe0b067cd8
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-06-05 00:20:31 +0000

    Updated close button sizeing and svh units

--- wave/src/LocationPage.js
+++ wave/src/LocationPage.js
@@ -1023,7 +1023,11 @@ function LocationPage(props) {
               <div id='comment-header'>
                 <div className='wave-wall'>Wave Stream</div>
                 <button className='close-button' onClick={closePopup}>
-                  &times;
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="23" height="23" viewBox="0 0 23 23" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M18 6l-12 12"/>
+                    <path d="M6 6l12 12"/>
+                  </svg>
                 </button>
               </div>
               
