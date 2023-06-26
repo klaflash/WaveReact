f6e1a2099c4cd3f73cb6511d0dac3fe5f6c19d92
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-06-26 04:02:05 +0000

    Styled trending score and timestamp

--- wave/src/MainPage.js
+++ wave/src/MainPage.js
@@ -625,43 +625,44 @@ function MainPage(props) {
                       <div className='bar-addy'>{location.addy}</div>
                     </div>
                     <div className='card-right-stack'>
-                      <div>
-                        {trending && trending[location.name] && trending[location.name] !== 0.0 ? (
-                          <>
-                            {trending[location.name] > 0 ? (
-                              <span className='trending-container'>
-                                <div className='trending-up'>{trending[location.name]}</div>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trending-up" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#569DAA" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                  <path d="M3 17l6 -6l4 4l8 -8"/>
-                                  <path d="M14 7l7 0l0 7"/>
-                                </svg>
-                              </span>
-                            ) : (
-                              <span className='trending-container'>
-                                <div className='trending-down'>{Math.abs(trending[location.name])}</div>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trending-down" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#C83200" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                  <path d="M3 7l6 6l4 -4l8 8"/>
-                                  <path d="M21 10l0 7l-7 0"/>
-                                </svg>
-                              </span>
-                            )}
-                          </>
-                        ) : (
-                          ''
-                        )}
-                      </div>
+                      
                       <div id='card-right-substack'>
                         {Object.keys(averages).length !== 0 && (
                           <div className='card-right-container'>
                             <div className='card-right'>{averages && averages[location.name] ? averages[location.name]['averageScore'] : ''}</div>
                           </div>
                         )}
-                        {Object.keys(mostRecent).length !== 0 && mostRecent[location.name] && (
+                        <div>
+                          {trending && trending[location.name] && trending[location.name] !== 0.0 ? (
+                            <>
+                              {trending[location.name] > 0 ? (
+                                <span className='trending-container'>
+                                  <div className='trending-up'>{trending[location.name]}</div>
+                                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trending-up" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#569DAA" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M3 17l6 -6l4 4l8 -8"/>
+                                    <path d="M14 7l7 0l0 7"/>
+                                  </svg>
+                                </span>
+                              ) : (
+                                <span className='trending-container'>
+                                  <div className='trending-down'>{Math.abs(trending[location.name])}</div>
+                                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trending-down" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#C83200" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M3 7l6 6l4 -4l8 8"/>
+                                    <path d="M21 10l0 7l-7 0"/>
+                                  </svg>
+                                </span>
+                              )}
+                            </>
+                          ) : (
+                            ''
+                          )}
+                        </div>
+                      </div>
+                      {Object.keys(mostRecent).length !== 0 && mostRecent[location.name] && (
                           <div className='timestamp'>{getTimeSince(mostRecent[location.name])}</div>
                         )}
-                      </div>
                     </div>
                   </div>
                   
