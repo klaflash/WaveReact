8bd59c1f18cfe53184e0ec5155b6db9b6b7f387d
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-07-04 01:51:39 +0000

    Imported locations from separate file

--- wave/src/MainPage.js
+++ wave/src/MainPage.js
@@ -52,18 +52,9 @@ function MainPage(props) {
   const locations = useMemo(() => props.locations, []);
 
   const [isMobile, setIsMobile] = useState(false);
-
   
   //const threshold = 0.09;
   const threshold = 100;
-  // const locations = useMemo(() => [
-  //   { name: 'Nova', latitude: 40.415171, longitude: -86.893275, addy: '200 S Fourth St', event: false, start: null, end: null, date: null, eventName: null, price: null},
-  //   { name: 'Hub', latitude: 40.422203, longitude: -86.906227, addy: '111 S Salisbury St', event: true, start: '2023-07-03T22:00-04:00', end: '2023-07-04T03:00-04:00', eventName: 'DJ whatever', price: '20'},
-  //   { name: 'Rise', latitude: 40.422677, longitude: -86.906967, addy: '134 W State St', event: true, start: '2023-07-04T22:00-04:00', end: '2023-07-04T23:00-04:00', eventName: 'Celebrity boxing', price: '10'},
-  //   { name: 'Test', latitude: 42.111683, longitude: -71.872295, addy: '123 Random St', event: true, start: '2023-07-04T22:00-04:00', end: '2023-07-05T01:00-04:00', eventName: 'Basketball vs IU', price: '2', buyLink: 'https://seatgeek.com/indiana-hoosiers-at-purdue-boilermakers-football-tickets/ncaa-football/2023-11-25-3-30-am/5853585'},
-  //   { name: 'Test2', latitude: 42.299103, longitude: -71.785020, addy: '123 Whatever Ave', event: true, start: '2023-07-07T21:00-04:00', end: '2023-07-07T24:00-04:00', eventName: 'Football vs Nebraska', price: 'Free'},
-  //   { name: 'Seattle', latitude: 47.607480, longitude: -122.336241, addy: '123 Whatever Ave', event: true, start: '2023-07-06T21:00-04:00', end: '2023-07-06T23:00-04:00', eventName: 'Football vs Nebraska', price: 'Free'}
-  // ], []);
 
   const handleLocationClick = useCallback((locationName) => {
     props.setCurrentLocation(locationName);
@@ -200,7 +191,7 @@ function MainPage(props) {
 
       const locationsByDistance = selectedDistance >= 0 ? sortedLocations.filter((location) => location.dist * 0.621371 <= selectedDistance) : sortedLocations;
       setFilteredLocations(locationsByDistance);
-      props.setLocations(sortedLocations)
+      //props.setLocations(sortedLocations)
     };
 
     const errorCallback = (error) => {
@@ -221,7 +212,7 @@ function MainPage(props) {
 
     runLocationUpdate()
 
-  }, [locations, props.setInRange, props.setLocations, threshold]);
+  }, [locations, props.setInRange, threshold]);
 
 
   async function updateAverages() {
---
+++ wave/src/locationsData.js
@@ -0,0 +1,73 @@
+const locations = [
+    {
+        "name": "Nova",
+        "latitude": 40.415171,
+        "longitude": -86.893275,
+        "addy": "200 S Fourth St",
+        "event": false,
+        "start": null,
+        "end": null,
+        "date": null,
+        "eventName": null,
+        "price": null
+    },
+    {
+        "name": "Hub",
+        "latitude": 40.422203,
+        "longitude": -86.906227,
+        "addy": "111 S Salisbury St",
+        "event": true,
+        "start": "2023-07-03T22:00-04:00",
+        "end": "2023-07-04T03:00-04:00",
+        "eventName": "DJ whatever",
+        "price": "20"
+    },
+    {
+        "name": "Rise",
+        "latitude": 40.422677,
+        "longitude": -86.906967,
+        "addy": "134 W State St",
+        "event": true,
+        "start": "2023-07-04T22:00-04:00",
+        "end": "2023-07-04T23:00-04:00",
+        "eventName": "Celebrity boxing",
+        "price": "10"
+    },
+    {
+        "name": "Test",
+        "latitude": 42.111683,
+        "longitude": -71.872295,
+        "addy": "123 Random St",
+        "event": true,
+        "start": "2023-07-04T22:00-04:00",
+        "end": "2023-07-05T01:00-04:00",
+        "eventName": "Basketball vs IU",
+        "price": "2",
+        "buyLink": "https://seatgeek.com/indiana-hoosiers-at-purdue-boilermakers-football-tickets/ncaa-football/2023-11-25-3-30-am/5853585"
+    },
+    {
+        "name": "Test2",
+        "latitude": 42.299103,
+        "longitude": -71.78502,
+        "addy": "123 Whatever Ave",
+        "event": true,
+        "start": "2023-07-07T21:00-04:00",
+        "end": "2023-07-07T24:00-04:00",
+        "eventName": "Football vs Nebraska",
+        "price": "Free"
+    },
+    {
+        "name": "Seattle",
+        "latitude": 47.60748,
+        "longitude": -122.336241,
+        "addy": "123 Whatever Ave",
+        "event": true,
+        "start": "2023-07-06T21:00-04:00",
+        "end": "2023-07-06T23:00-04:00",
+        "eventName": "Football vs Nebraska",
+        "price": "Free"
+    }
+]
+
+
+  export default locations;
