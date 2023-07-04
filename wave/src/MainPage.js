d958598f9e9d64c290a25bc973d5dcaa0ea57712
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-07-04 00:30:47 +0000

    Changed event times to timestamps

--- wave/src/MainPage.js
+++ wave/src/MainPage.js
@@ -55,11 +55,11 @@ function MainPage(props) {
   const threshold = 100;
   const locations = useMemo(() => [
     { name: 'Nova', latitude: 40.415171, longitude: -86.893275, addy: '200 S Fourth St', event: false, start: null, end: null, date: null, eventName: null, price: null},
-    { name: 'Hub', latitude: 40.422203, longitude: -86.906227, addy: '111 S Salisbury St', event: true, start: '9', end: '12pm', date: 'Mar 11', eventName: 'DJ whatever', price: '20'},
-    { name: 'Rise', latitude: 40.422677, longitude: -86.906967, addy: '134 W State St', event: true, start: '9', end: '12pm', date: 'Mar 30', eventName: 'Celebrity boxing', price: '10'},
-    { name: 'Test', latitude: 42.111683, longitude: -71.872295, addy: '123 Random St', event: true, start: '9', end: '12pm', date: 'Mar 1', eventName: 'Basketball vs IU', price: '2', buyLink: 'https://seatgeek.com/indiana-hoosiers-at-purdue-boilermakers-football-tickets/ncaa-football/2023-11-25-3-30-am/5853585'},
-    { name: 'Test2', latitude: 42.299103, longitude: -71.785020, addy: '123 Whatever Ave', event: true, start: '9', end: '12pm', date: 'Jun 5', eventName: 'Football vs Nebraska', price: 'Free'},
-    { name: 'Seattle', latitude: 47.607480, longitude: -122.336241, addy: '123 Whatever Ave', event: true, start: '9', end: '12pm', date: 'Mar 8', eventName: 'Football vs Nebraska', price: 'Free'}
+    { name: 'Hub', latitude: 40.422203, longitude: -86.906227, addy: '111 S Salisbury St', event: true, start: '2023-07-03T22:00-04:00', end: '2023-07-03T23:00-04:00', eventName: 'DJ whatever', price: '20'},
+    { name: 'Rise', latitude: 40.422677, longitude: -86.906967, addy: '134 W State St', event: true, start: '2023-07-04T22:00-04:00', end: '2023-07-04T23:00-04:00', eventName: 'Celebrity boxing', price: '10'},
+    { name: 'Test', latitude: 42.111683, longitude: -71.872295, addy: '123 Random St', event: true, start: '2023-07-04T22:00-04:00', end: '2023-07-05T01:00-04:00', eventName: 'Basketball vs IU', price: '2', buyLink: 'https://seatgeek.com/indiana-hoosiers-at-purdue-boilermakers-football-tickets/ncaa-football/2023-11-25-3-30-am/5853585'},
+    { name: 'Test2', latitude: 42.299103, longitude: -71.785020, addy: '123 Whatever Ave', event: true, start: '2023-07-07T21:00-04:00', end: '2023-07-07T24:00-04:00', eventName: 'Football vs Nebraska', price: 'Free'},
+    { name: 'Seattle', latitude: 47.607480, longitude: -122.336241, addy: '123 Whatever Ave', event: true, start: '2023-07-06T21:00-04:00', end: '2023-07-06T23:00-04:00', eventName: 'Football vs Nebraska', price: 'Free'}
   ], []);
 
   const handleLocationClick = useCallback((locationName) => {
@@ -806,7 +806,29 @@ function MainPage(props) {
                           <path d="M12 12l-3 -2"/>
                           <path d="M12 7v5"/>
                         </svg>
-                        <div>{location.start + ' - ' + location.end + ' ' + location.date}</div>
+                        <div>
+                          {(() => {
+                            const startTime = new Date(location.start);
+                            const endTime = new Date(location.end);
+
+                            console.log(startTime)
+                            console.log(endTime)
+
+                            const startHour = startTime.getHours() % 12 || 12;
+                            const endHour = endTime.getHours() % 12 || 12;
+
+                            const startPeriod = startTime.getHours() >= 12 ? 'pm' : 'am';
+                            const endPeriod = endTime.getHours() >= 12 ? 'pm' : 'am';
+
+                            const formattedDate = startTime.toLocaleString('en-US', {
+                              month: 'short',
+                              day: 'numeric'
+                            });
+
+                            return `${startHour} - ${endHour}${endPeriod} ${formattedDate}`;
+                          })()}
+                        </div>
+
                       </div>
                       <div className='bar-name-small-line'>
                         <div className="circle"></div>
