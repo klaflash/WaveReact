1eab5aff37fa4db98fbbc2d4193ece0f85bb0bee
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-07-24 05:26:01 +0000

    Added iphone 12 pro splash

--- wave/src/MainPage.js
+++ wave/src/MainPage.js
@@ -801,7 +801,7 @@ function MainPage(props) {
 
           </div>
 
-          <div id='explore'>Find The Waves</div>
+          <div id='explore'>Find The Wave</div>
 
           <div id='search-bar'>
             <input
